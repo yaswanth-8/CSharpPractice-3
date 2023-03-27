734673e2a7b78aebe2e4aa147700c48be11c8162
yaswanth <ya8wanth@gmail.com>
2023-03-27 12:43:38 +0530

    Interface implemented

---
+++ CSharpPractice-3/Interface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice_3
+{
+    public interface IParent {
+        void show();
+    }
+
+    class derivedClass : IParent {
+        public void show()
+        {
+            Console.WriteLine("inside show of interface");
+        }
+    }
+
+}
--- CSharpPractice-3/oops.cs
+++ CSharpPractice-3/oops.cs
@@ -18,8 +18,10 @@ namespace CSharpPractice_3
           // Marks marks = new Marks();
           //  marks.science = 88;
           // Student agg = new Student(marks);
-          Derived abs = new Derived();
-            abs.method("yaswanth");
+         // Derived abs = new Derived();
+            //abs.method("yaswanth");
+            derivedClass dc = new derivedClass();
+            dc.show();
         }
     }
 }
